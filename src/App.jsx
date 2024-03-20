b6505865e5cbeea5adcf402fe81e1b2d3654ebd0
ramraj <ramrajsainiiitd>
2024-03-20 19:16:09 +0530

    changes done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div id="main">
+      <p>
+      Now I can render any React component on any DOM node I want using ReactDOM.render
+      </p>
+    </div>
+    </>  
+  )
+}
+
+export default App
--- src/components/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-    return (
-        <div id="main">
-            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-        </div>
-    );
-}
-
-export default App;
