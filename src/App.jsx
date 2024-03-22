13a3b7f792c9b74f6caf164b810e0f31334a63da
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-22 13:03:46 +0000

    fixed issue

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,11 @@ import './App.css'
 function App() {
 
   return (
-    <>
     <div id="main">
       <p>
       Now I can render any React component on any DOM node I want using ReactDOM.render
       </p>
-    </div>
-    </>  
+    </div> 
   )
 }
 
