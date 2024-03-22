1e6c9c4f6f1f5f7f217ce4593c4c38bbba1dc48e
ramraj saini <ramrajsainiiitd@gmail.com>
2024-03-22 18:56:59 +0530

    updatedCode

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 // import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
   return (
-    <>
     <div id="main">
       <p>
       Now I can render any React component on any DOM node I want using ReactDOM.render
       </p>
     </div>
-    </>  
   )
 }
 
